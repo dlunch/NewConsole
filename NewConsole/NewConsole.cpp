240c351b97da214d7eb214846eefb7cec75b035a
Inseok Lee <dlunch@gmail.com>
2014-02-12 15:51:07 +0900

    Fix newline.

--- NewConsole/NewConsole.cpp
+++ NewConsole/NewConsole.cpp
@@ -147,6 +147,8 @@ LRESULT NewConsole::WndProc(UINT iMessage, WPARAM wParam, LPARAM lParam)
 			if(len > 0)
 			{
 				temp.resize(len);
+				if(temp[0] == L'\r' && len == 1)
+					temp[0] = L'\n';
 				activeConsole_.lock()->appendInputBuffer(temp);
 			}
 			else
